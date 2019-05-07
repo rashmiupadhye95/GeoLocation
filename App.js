731ec4ea825762d6ae0cb6b543b3fb19b51e057f
Rashmi Upadhye <rashmi11395@gmail.com>
2019-05-07 15:44:29 +0530

    gggg

--- App.js
+++ App.js
@@ -6,21 +6,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, Dimensions, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Dimensions, StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 const SCREEN_HEIGHT = height
 const SCREEN_WIDTH = width
-const ASPECT_RATIO = height/width
+const ASPECT_RATIO = height / width
 const LATTITUDE_DELTA = 0.009
 const LONGITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO;
+
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 12.9158382, longitude: 77.6080513};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCFSoMZOk1MMoF8goBH_q0iZuNX1d1gUrg';
 
 
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -34,60 +39,67 @@ export default class App extends Component {
       markerPosition: {
         latitude: 0,
         longitude: 0
-      },
+      }
     }
   }
 
   watchID = null;
 
-  componentDidMount(){
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition((position) => {
       var lat = parseFloat(position.coords.latitude)
       var long = parseFloat(position.coords.longitude)
-      
-      var initialRegion= {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA, 
-      }
-      this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
 
-      var lastRegion = {
+      var initialRegion = {
         latitude: lat,
         longitude: long,
         latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA, 
+        longitudeDelta: LONGITUDE_DELTA,
       }
-      this.setState({initialPosition: lastRegion})
-      this.setState({markerPosition: lastRegion})
-    })
+      this.setState({ initialPosition: initialRegion })
+      this.setState({ markerPosition: initialRegion })
+    },
+      (error) => alert(JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 60 * 60 },
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
+
+        var lastRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATTITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+        this.setState({ initialPosition: lastRegion })
+        this.setState({ markerPosition: lastRegion })
+      })
     )
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     navigator.geolocation.clearWatch(this.watchID)
   }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
-        style={styles.map}
-        region={this.state.initialPosition}>
+          style={styles.map}
+          region={this.state.initialPosition}>
           <MapView.Marker
-          coordinate={this.state.markerPosition}>
+            coordinate={this.state.markerPosition}>
             <View style={styles.radius}>
-            <View style={styles.marker}/>
-
+              <View style={styles.marker} />
             </View>
           </MapView.Marker>
+          <MapViewDirections
+          origin={this.state.initialPosition}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={6}
+          strokeColor='#2971FF'
+          />
         </MapView>
       </View>
     );
@@ -111,7 +123,7 @@ const styles = StyleSheet.create({
   radius: {
     height: 50,
     width: 50,
-    borderRadius: 50/2,
+    borderRadius: 50 / 2,
     overflow: 'hidden',
     backgroundColor: 'rgba(0, 122, 255, 0.1)',
     borderWidth: 1,
@@ -124,7 +136,7 @@ const styles = StyleSheet.create({
     width: 20,
     borderWidth: 3,
     borderColor: 'white',
-    borderRadius: 20/2,
+    borderRadius: 20 / 2,
     overflow: 'hidden',
     backgroundColor: '#007AFF'
   }
